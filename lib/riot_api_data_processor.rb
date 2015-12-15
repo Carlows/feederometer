a8877316cba1ef9d89bc1ce59899f8c1d540a2cb
Carlos <carlosjesus94@gmail.com>
2015-12-15 01:16:47 -0430

    Added a class to calculate the kda and the percentage of matches where players feed

--- lib/riot_api_data_processor.rb
+++ lib/riot_api_data_processor.rb
@@ -1,12 +1,10 @@
 require_relative "riot_api_requests_json"
-require 'pp'
 
-class RiotApiDataProcesor
-	def initialize()
+class RiotApiDataProcessor
+	def initialize
 		@riot_api = RiotApiRequestsJSON.new('73cb4b46-ae25-440f-bb17-da66159eff9b')
 	end
 
-	# summoner name needs to be in lower case
 	def get_recent_games_data_for_summoner_name(summoner_name)
 		summoner_name = summoner_name.downcase
 
@@ -17,20 +15,13 @@ class RiotApiDataProcesor
 		recent_games_summoner_data = {
 			"name" => summoner_data[summoner_name]["name"],
 			"icon_id" => summoner_data[summoner_name]["profileIconId"],
-			"games" => recent_games_data["games"].map { | item |
-				{ 
-					"champion_id" => item["championId"],
-					"stats" => {
-						"deaths" => item["stats"]["numDeaths"],
-						"assists" => item["stats"]["assists"],
-						"kills" => item["stats"]["championsKilled"]
-					}
+			"stats_games" => recent_games_data["games"].map do | item |
+				{
+					"deaths" => item["stats"]["numDeaths"],
+					"assists" => item["stats"]["assists"],
+					"kills" => item["stats"]["championsKilled"]
 				}
-			}
-		}
+			end
+		}		
 	end
 end
-
-#api_test = RiotApiDataProcesor.new
-
-#pp api_test.get_recent_games_data_for_summoner_name("CARLOSEME")
