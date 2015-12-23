6316bd40c9a9c0598cd9b1b6b3006e448cb5e21e
Carlos <carlosjesus94@gmail.com>
2015-12-23 01:52:19 -0430

    retrieving key

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'feeder' => 'feeder#find'
   get 'feeder_team' => 'feeder#find_team'
 
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
