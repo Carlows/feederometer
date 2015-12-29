20f18155c91119d8db9f869a820a3421d117f7d1
Carlos <carlosjesus94@gmail.com>
2015-12-29 17:07:31 -0430

    Added team script

--- public/feederometer_team.js
+++ public/feederometer_team.js
@@ -3,17 +3,31 @@ $(document).ready(function(){
 		var panel = $('.panel-team')
 	    var championName = panel.attr('data-champion');
 
-	    panel.css('background-image', 'url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + championName + '_0.jpg)')
+	    panel.css('background-image', 'url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + championName + '_0.jpg)');
 	}
 
 	set_background();
 
 	$('.feederometer-team-bar').each(function(){
-		var feedPercentage = $(this).attr('data-feed');
-		$(this).children('.custom-bar').animate({ width: feedPercentage + "%" }, 2000);
+		var currentBar = $(this);
+		var feedPercentage = currentBar.attr('data-feed');
+		currentBar.children('.custom-bar').animate({ width: feedPercentage + "%" }, 
+		1500, function(){
+			// animation completed
+			if(feedPercentage <= 0){
+				currentBar.next().text(parseInt(feedPercentage) + "%! Such Tryhard");				
+			}
+			else if(feedPercentage > 0 && feedPercentage <= 60){
+				currentBar.addClass('normal');
+				currentBar.parents('.player').addClass('normal');
+				currentBar.next().text(parseInt(feedPercentage) + "%!");				
+			}
+			else if(feedPercentage > 60){
+				currentBar.addClass('feeder');
+				currentBar.parents('.player').addClass('feeder');
+				currentBar.next().text(parseInt(feedPercentage) + "%! REPORT!!!");
+			}
+		});
 
-		if(feedPercentage > 20){
-			$(this).parents('.player').addClass('feeder');
-		}
 	});
-}
+});
